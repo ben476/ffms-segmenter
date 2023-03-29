2291524c1630bf3a55e5d681d91c2843924db387
Benjamin Hong <benjamin.hong476@gmail.com>
2023-03-29 17:28:06 +1300

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use ffms2::frame::{Frame, Resizers};
+use ffms2::frame::Frame;
 use ffms2::video::VideoSource;
 use std::fs::File;
-use std::io::{ErrorKind, Write};
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::time::Instant;
 use std::{thread, time};
 use structopt::StructOpt;
-use y4m::{encode, Colorspace, Encoder, Frame as Y4MFrame, Ratio};
+use y4m::{encode, Colorspace, Frame as Y4MFrame, Ratio};
 
 use ffms2::index::*;
 use ffms2::track::*;
@@ -22,28 +21,12 @@ macro_rules! print_progress {
 
 #[derive(Debug, StructOpt)]
 struct CliArgs {
-    /// Force overwriting of existing index file, if any
-    #[structopt(short = "f", long = "force")]
-    force: bool,
-    /// The first frame to extract
-    #[structopt(short = "b", long = "start", default_value = "0")]
-    start: usize,
-    /// The last frame to extract
-    /// (0 means last frame)
-    #[structopt(short = "e", long = "end", default_value = "0")]
-    end: usize,
     /// Set FFmpeg verbosity level
     #[structopt(short = "v", long = "verbose", default_value = "0")]
     verbose: usize,
     /// Disable progress reporting
     #[structopt(short = "p", long = "progress")]
     progress: bool,
-    /// Write timecodes for all video tracks to outputfile_track00.tc.txt
-    #[structopt(short = "c", long = "timecodes")]
-    timecodes: bool,
-    /// Write keyframes for all video tracks to outputfile_track00.kf.txt
-    #[structopt(short = "k", long = "keyframes")]
-    keyframes: bool,
     /// Set the audio indexing mask to N
     /// (-1 means index all tracks, 0 means index none)
     #[structopt(short = "t", long = "index", default_value = "0")]
@@ -74,34 +57,8 @@ fn update_progress(current: usize, total: usize, private: Option<&mut usize>) ->
     0
 }
 
-#[inline]
-fn dump_filename(track: &Track, track_num: usize, cache_file: &Path, suffix: &str) -> PathBuf {
-    if track.NumFrames() == 0 {
-        return PathBuf::new();
-    }
-
-    if let TrackType::TYPE_VIDEO = track.TrackType() {
-        let start = cache_file.to_str().unwrap();
-        let filename = format!("{}_track{:02}{}", start, track_num, suffix);
-        PathBuf::from(filename)
-    } else {
-        PathBuf::new()
-    }
-}
-
-fn do_indexing(
-    args: &CliArgs,
-    // cache_file: &Path,
-    ignore_errors: IndexErrorHandling,
-) -> std::io::Result<()> {
+fn do_indexing(args: &CliArgs, ignore_errors: IndexErrorHandling) -> std::io::Result<()> {
     let mut progress = 0;
-
-    // if cache_file.exists() && !args.force {
-    //     panic!(
-    //         "Error: index file already exists, \
-    //          use -f if you are sure you want to overwrite it."
-    //     );
-    // }
 
     let indexer = Indexer::new(&args.input_file).unwrap();
 
@@ -121,42 +78,6 @@ fn do_indexing(
     }
 
     let index = indexer.DoIndexing2(ignore_errors).unwrap();
-
-    // if args.timecodes {
-    //     print_progress!(args.progress, "Writing timecodes...");
-    //     let num_tracks = index.NumTracks();
-    //     for t in 0..num_tracks {
-    //         let track = Track::TrackFromIndex(&index, t);
-    //         let filename = dump_filename(&track, t, &cache_file, ".tc.txt");
-    //         if !filename.to_str().unwrap().is_empty() && track.WriteTimecodes(&filename).is_err() {
-    //             eprintln!(
-    //                 "Failed to write timecodes file {}",
-    //                 filename.to_str().unwrap()
-    //             );
-    //         }
-    //     }
-    //     print_progress!(args.progress, "Done.");
-    // }
-
-    // if args.keyframes {
-    //     print_progress!(args.progress, "Writing keyframes...");
-    //     let num_tracks = index.NumTracks();
-    //     for t in 0..num_tracks {
-    //         let track = Track::TrackFromIndex(&index, t);
-    //         let filename = dump_filename(&track, t, &cache_file, ".kf.txt");
-    //         if !filename.to_str().unwrap().is_empty() {
-    //             let mut file = File::create(filename)?;
-    //             write!(file, "# keyframe format v1\nfps 0\n")?;
-    //             let frame_count = track.NumFrames();
-    //             for f in 0..frame_count {
-    //                 if track.FrameInfo(f).KeyFrame() != 0 {
-    //                     writeln!(file, "{}", f)?;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     print_progress!(args.progress, "Done.");
-    // }
 
     print_progress!(args.progress, "Video indexed!");
 
@@ -189,12 +110,8 @@ fn do_indexing(
     eprintln!("Pixel format: {}", prop_frame.ConvertedPixelFormat);
 
     let yuv420p = Frame::GetPixFmt("yuv420p");
-    let yuyv422 = Frame::GetPixFmt("yuyv422");
     let yuv422p = Frame::GetPixFmt("yuv422p");
-    let rgb24 = Frame::GetPixFmt("rgb24");
     let yuv420p10le = Frame::GetPixFmt("yuv420p10le");
-
-    let ref mut pix_fmts = vec![yuv420p, yuv420p10le];
 
     let width = prop_frame.EncodedWidth as usize;
     let height = prop_frame.EncodedHeight as usize;
@@ -216,10 +133,6 @@ fn do_indexing(
     video_source
         .SetInputFormatV(1 as usize, video::ColorRanges::CR_MPEG, yuv420p as usize)
         .unwrap();
-
-    // video_source
-    //     .SetOutputFormatV2(pix_fmts, width, height, Resizers::RESIZER_BICUBIC)
-    //     .unwrap();
 
     thread::sleep(time::Duration::from_millis(100));
 
@@ -258,21 +171,13 @@ fn do_indexing(
 
     eprintln!("Line size: {:?}", line_size);
 
-    // let raw_array: [*const u8; 4] = prop_frame.Data;
-    // let slice_array: [&[u8]; 3] = unsafe {
-    //     std::mem::transmute(raw_array)
-    // };
-
-    // while loop read string from stdin
     let mut input = String::new();
     loop {
         input.clear();
-        // read a line from standard input
         let _ = std::io::stdin().read_line(&mut input);
 
         let now = Instant::now();
 
-        // trim the trailing newline
         input = input.trim().to_string();
 
         let start_end_str = input.split(" ").collect::<Vec<&str>>();
@@ -313,32 +218,11 @@ fn do_indexing(
         for i in start..end {
             let mut frame = Frame::GetFrame(video_source, i).unwrap();
 
-            // frame.set_LineSize(&line_size);
-            // Frame::set_LineSize(&mut frame, &line_size);
-
+            // Work around for bug in FFMS2 Rust bindings
             frame.Linesize[1] /= 2;
             frame.Linesize[2] /= 2;
 
-            // eprintln!("Line size: {:?}", frame.Linesize);
-
-            // frame.Linesize[1] = 480;
-            // frame.Linesize[2] = 480;
-
-            // eprintln!("Frame colorspace: {}", frame.ColorSpace);
-
             let pixel_data: Vec<Option<&[u8]>> = frame.get_pixel_data();
-
-            // eprintln!("Y plane length: {:?}", pixel_data[0].unwrap().len());
-            // eprintln!("U plane length: {:?}", pixel_data[1].unwrap().len());
-            // eprintln!("V plane length: {:?}", pixel_data[2].unwrap().len());
-
-            // save each plane to a file
-            // let mut y_file = File::create(format!("y_plane_{}.bin", i)).unwrap();
-            // y_file.write_all(pixel_data[0].unwrap()).unwrap();
-            // let mut u_file = File::create(format!("u_plane_{}.bin", i)).unwrap();
-            // u_file.write_all(pixel_data[1].unwrap()).unwrap();
-            // let mut v_file = File::create(format!("v_plane_{}.bin", i)).unwrap();
-            // v_file.write_all(pixel_data[2].unwrap()).unwrap();
 
             let frame = Y4MFrame::new(
                 [
@@ -349,25 +233,6 @@ fn do_indexing(
                 None,
             );
 
-            // get xor of all planes
-            // let mut xor = 0;
-            // for plane in pixel_data {
-            //     if let Some(plane) = plane {
-            //         for byte in plane {
-            //             xor ^= *byte;
-            //         }
-            //     }
-            // }
-
-            // eprintln!("XOR: {}", xor);
-
-            // let mut outfile = File::create("out/".to_owned() + &i.to_string() + ".y4m").unwrap();
-
-            // let mut encoder = encode(width, height, framerate)
-            //     .with_colorspace(y4m_colorspace)
-            //     .write_header(&mut outfile)
-            //     .unwrap();
-
             encoder.write_frame(&frame).unwrap();
         }
 
@@ -375,37 +240,10 @@ fn do_indexing(
 
         println!("{} {}", start, outpath);
     }
-    // print_progress!(args.progress, "Done.");
-
-    Ok(())
 }
 
 fn main() {
     let args = CliArgs::from_args();
-
-    eprintln!("{:?}", Frame::GetPixFmt("yuv420p"));
-    eprintln!("{:?}", Frame::GetPixFmt("yuyv422"));
-    eprintln!("{:?}", Frame::GetPixFmt("yuv422p"));
-    eprintln!("{:?}", Frame::GetPixFmt("rgb24"));
-    eprintln!("{:?}", Frame::GetPixFmt("yuv420p10le"));
-
-    // if args.ignore_errors > 3 {
-    //     panic!("Error: invalid audio decoding error handling mode");
-    // }
-
-    // let cache_file = if let Some(out) = &args.output_file {
-    //     out.to_path_buf()
-    // } else {
-    //     let file_stem = args
-    //         .input_file
-    //         .as_path()
-    //         .file_stem()
-    //         .unwrap()
-    //         .to_str()
-    //         .unwrap();
-    //     let filename = format!("{}.ffindex", file_stem);
-    //     Path::new(&filename).to_path_buf()
-    // };
 
     FFMS2::Init();
 
